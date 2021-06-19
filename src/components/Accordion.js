e65d7f1041e712f0552314960013ba8a8244990b
KPMGE <jesuskevin254@gmail.com>
2021-06-19 17:05:26 -0300

    Finishing accordion

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,7 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Accordion = ({ items }) => {
-  return <h2>There are {items.length} items</h2>;
+  const [activeIndex, setActiveIndex] = useState(null);
+
+  const onTitleClickHandler = (index) => {
+    setActiveIndex(index);
+  };
+
+  const itemsToRender = items.map((item, index) => {
+    const active = index === activeIndex ? "active" : "";
+
+    return (
+      <React.Fragment>
+        <div
+          className={`title ${active}`}
+          onClick={() => onTitleClickHandler(index)}
+        >
+          <i className="dropdown icon"></i>
+          {item.title}
+        </div>
+
+        <div className={`content ${active}`}>
+          <p>{item.content}</p>
+        </div>
+      </React.Fragment>
+    );
+  });
+
+  return <div className="ui styled accordion">{itemsToRender}</div>;
 };
 
 export default Accordion;
